d82fc6ddc0f39ab3625fc65d7d404cd331525a91
Ashutosh <ashutosh211gupta@gmail.com>
2024-06-16 21:11:04 +0530

    Initial commit

---
+++ src/main/java/com/bookwise/backend/BookwiseLmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bookwise.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwiseLmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwiseLmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookwise/backend/BookwiseLmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookwise.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookwiseLmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
