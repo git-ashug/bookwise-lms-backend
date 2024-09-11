64a5c266f4435726975cfb0c2a87af0912002a07
Ashutosh <ashutosh211gupta@gmail.com>
2024-09-11 22:32:28 +0530

    Exposed some JPA Query Methods for frontend

--- src/main/java/com/bookwise/backend/dao/BookRepository.java
+++ src/main/java/com/bookwise/backend/dao/BookRepository.java
@@ -1,9 +1,15 @@
 package com.bookwise.backend.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bookwise.backend.entities.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+	Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+	
+	Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }
